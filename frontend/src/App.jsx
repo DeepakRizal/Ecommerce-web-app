3837c53a3f67c1b942b6f75ae9c24b394f953553
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-15 15:39:48 +0530

    feat: complete the ui portion of the profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collection/:collection" element={<CollectionPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
